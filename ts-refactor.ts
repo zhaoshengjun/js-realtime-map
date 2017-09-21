b107e1eba87b7f4db37b3a2c945e1fcc98e7c911
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-22 09:43:32 +1000

    initial state

--- ts-refactor.ts
+++ ts-refactor.ts
@@ -5,3 +5,13 @@ export function volume(radius: number, height: number) {
   if (radius_sq < 0 && height > 0) return null;
   return base_area * height;
 }
+
+
+//quick fix
+
+var x: *;
+var y: number;
+interface quote {
+  bar: number;
+}
+var z: quote.bar;
